c16042b628d04ef42a806f924e05ead4db2b5c99
StepanIP <scrupnichuk@gmail.com>
2024-05-01 12:29:28 +0300

    Initial commit

---
+++ src/main/java/org/example/cryptodealbottesttask/CryptoDealBotTestTaskApplication.java
@@ -0,0 +1,13 @@
+package org.example.cryptodealbottesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoDealBotTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoDealBotTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cryptodealbottesttask/CryptoDealBotTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cryptodealbottesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoDealBotTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
